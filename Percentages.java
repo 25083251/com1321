8f39c1771f42c602dc4688525fa185af1fc5a7bd
Cleophus Tshinyelani <25083251@mvula.univen.ac.za>
2026-08-24 14:02:31 +0200

    shoould
    hello

--- Percentages.java
+++ Percentages.java
@@ -1,17 +1,13 @@
-import java.util.Scanner;
-
 public class Percentages {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the numbers (A)");
-        double num1 = input.nextDouble();
-        System.out.println("Enter the numbers (B)");
-        double num2 = input.nextDouble();
+        double num1 = 100.0;
+        double num2 = 1000.0;
+
         computePercent(num1, num2);
     }
 
     public static void computePercent(double num1, double num2) {
         double percent = (num1 / num2) * 100;
-        System.out.println("the percent is " + percent);
+        System.out.println("The percent is :" + percent);
     }
 }
